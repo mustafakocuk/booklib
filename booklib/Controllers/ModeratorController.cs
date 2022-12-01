01248841b5116d58f122f197a1a42b4003415f6b
mustafakocuk <mustafakocuk19@gmail.com>
2022-12-01 16:11:39 +0300

    Add project files.

---
+++ booklib/Controllers/ModeratorController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using booklib.Entities;
+using booklib.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace booklib.Controllers
+{
+    public class ModeratorController : Controller
+    {
+        private readonly DatabaseContext _databaseContext;
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+
+        public ModeratorController(DatabaseContext databaseContext, IConfiguration configuration, IMapper mapper)
+        {
+            _databaseContext = databaseContext;
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(BookModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if(_databaseContext.Books.Any(x => x.BookName.ToLower() == model.BookName.ToLower() && x.Author.ToLower() == model.Author.ToLower()))
+                {
+                    ModelState.AddModelError(nameof(model.BookName), "Kitap mevcut, Stok arttırımı yapabilirsiniz.");
+                    return View(model);
+                }
+                //Book book = new Book()
+                //{
+                //    BookName = model.BookName,
+                //    Author = model.Author,
+                //    BookType = model.BookType,
+                //    //BookImageFileName = model.BookImageFileName,
+                //    BookSubject = model.BookSubject,
+                //    PublishingDate = model.PublishingDate,
+                //    Stock = model.Stock
+                //};
+
+                Book book = _mapper.Map<Book>(model);
+
+                _databaseContext.Books.Add(book);
+                int affectedRowCount = _databaseContext.SaveChanges();
+
+                if (affectedRowCount == 0)
+                {
+                    ModelState.AddModelError("", "Kitap eklenememiştir.");
+                }
+                else
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ booklib/Models/BookViewModels.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace booklib.Models
+{
+    public class BookModel
+    {
+        public Guid Id { get; set; }
+
+        public string BookName { get; set; }
+
+        public string BookType { get; set; }
+
+        public string BookSubject { get; set; }
+
+        public string Author { get; set; }
+
+        public int Stock { get; set; }
+
+        public string? BookImageFileName { get; set; }
+
+        public DateTime PublishingDate { get; set; }
+    }
+
+    public class BookCreateModel
+    {
+        public string BookName { get; set; }
+
+        public string BookType { get; set; }
+
+        public string BookSubject { get; set; }
+
+        public string Author { get; set; }
+
+        public int Stock { get; set; }
+
+        public string BookImageFileName { get; set; }
+
+        public DateTime PublishingDate { get; set; }
+    }
+
+    public class BookEditModel
+    {
+        public string BookName { get; set; }
+
+        public string BookType { get; set; }
+
+        public string BookSubject { get; set; }
+
+        public string Author { get; set; }
+
+        public int Stock { get; set; }
+
+        public string BookImageFileName { get; set; }
+
+        public DateTime PublishingDate { get; set; }
+    }
+
+}
